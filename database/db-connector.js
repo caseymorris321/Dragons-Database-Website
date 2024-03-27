d788f77c5601aff51802e608b316a048d3ad4719
Casey Morris <morrisacasey@gmail.com>
2024-03-27 11:15:36 -0400

    changed to postgres db

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,14 +1,27 @@
-// Get an instance of mysql we can use in the app
-var mysql = require('mysql')
+// // Get an instance of mysql we can use in the app
+// var mysql = require('mysql')
 
-// Create a 'connection pool' using the provided credentials
-var pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : '127.0.0.1',
-    user            : 'root',
-    password        : '3876',
-    database        : 'dragons'
-})
+// // Create a 'connection pool' using the provided credentials
+// var pool = mysql.createPool({
+//     connectionLimit : 10,
+//     host            : 'process.env.DB_HOST',
+//     user            : 'process.env.DB_USER',
+//     password        : 'process.env.DB_PASSWORD',
+//     database        : 'process.env.DB_NAME'
+// })
 
-// Export it for use in our application
-module.exports.pool = pool;
+// // Export it for use in our application
+// module.exports.pool = pool;
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    max: 10, // Set pool max size to 10
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+module.exports.pool = pool
